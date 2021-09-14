cef8a585a26771edc12faa70f820bb393c45835c
RuslanGRA <online0180@mail.ru>
2021-09-14 22:55:32 +0300

    файл с ошибкой удален

--- src/main/java/ru/job4j/ru/job4j/StudentInfo.java
+++
@@ -1,8 +0,0 @@
-package ru.job4j.ru.job4j;
-
-public class StudentInfo {
-    public static void main(String[] args) {
-        System.out.println("Ruslan Gaitarov");
-        System.out.println("12.11.1983");
-    }
-}
