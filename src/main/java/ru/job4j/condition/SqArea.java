fb155e6226255a32c0de872b5f0eb13a3a11f8b5
RuslanGRA <online0180@mail.ru>
2021-10-30 21:03:00 +0300

    стороны прямоугольника

--- src/main/java/ru/job4j/condition/SqArea.java
+++ src/main/java/ru/job4j/condition/SqArea.java
@@ -5,8 +5,7 @@ public class SqArea {
         double h = p / (2 * (k + 1));
         double l = h * k;
         double s = l * h;
-        double rsl = s;
-        return rsl;
+        return s;
         }
 
     public static void main(String[] args) {
