8d82e4e8ced19870af153f7a845c3afd7e84f1ba
RuslanGRA <online0180@mail.ru>
2021-11-15 16:13:17 +0300

    String

---
+++ src/main/java/ru/job4j/condition/Greeting.java
@@ -0,0 +1,12 @@
+package ru.job4j.condition;
+
+public class Greeting {
+    public static void main(String[] args) {
+        String idea = "I like Java!";
+        System.out.println(idea);
+        String newbie = "But I am a newbie ";
+        int year = 2021;
+        idea = newbie + year;
+        System.out.println(idea);
+    }
+}
