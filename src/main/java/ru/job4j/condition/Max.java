49dc9203798e06d6ca89f18d6cd363a5ac141a57
RuslanGRA <online0180@mail.ru>
2022-01-23 20:16:13 +0300

    тернарное сравнение и тесты к нему

--- src/main/java/ru/job4j/condition/Max.java
+++ src/main/java/ru/job4j/condition/Max.java
@@ -3,7 +3,11 @@ package ru.job4j.condition;
 public class Max {
     public static int max(int left, int right) {
         boolean condition = true;
-        int result = condition ? left : right;
-        return result;
+        return left >= right ? left : right;
+    }
+
+    public static void main(String[] args) {
+        int fin = Max.max(1, 0);
+        System.out.println(fin);
     }
 }
--- src/test/java/ru/job4j/condition/MaxTest.java
+++ src/test/java/ru/job4j/condition/MaxTest.java
@@ -6,11 +6,11 @@ import org.junit.Assert;
 public class MaxTest {
 
     @Test
-    public void whenMax1To0Then1() {
+    public void whenMax1To2Then2() {
         int left = 1;
-        int right = 0;
+        int right = 2;
         int result = Max.max(left, right);
-        int expected = 1;
+        int expected = 2;
         Assert.assertEquals(result, expected);
     }
 
@@ -24,11 +24,11 @@ public class MaxTest {
     }
 
     @Test
-    public void whenMax2To3Then2() {
+    public void whenMax2To3Then3() {
         int left = 2;
         int right = 3;
         int result = Max.max(left, right);
-        int expected = 2;
+        int expected = 3;
         Assert.assertEquals(result, expected);
     }
 
