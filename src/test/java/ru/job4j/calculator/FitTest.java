be9f318ec3274b7e56a0ddd0967f9ec0d6b4a85c
Ruslan <online0180@mail.ru>
2025-01-20 16:06:46 +0300

    удаление однострочных комментов

--- src/test/java/ru/job4j/calculator/FitTest.java
+++ src/test/java/ru/job4j/calculator/FitTest.java
@@ -8,16 +8,16 @@ public class FitTest {
     @Test
     public void whenManHeight187ThenWeight100() {
         short height = 187;
-        double expected = (187 - 100) * 1.15; // 100.05
+        double expected = (187 - 100) * 1.15;
         double actual = Fit.idealWeight(height, true);
-        double delta = 0.01; // Точность округления
+        double delta = 0.01;
         assertEquals(expected, actual, delta);
     }
 
     @Test
     public void whenWomanHeight170ThenWeight69() {
         short height = 170;
-        double expected = (170 - 110) * 1.15; // 69.00
+        double expected = (170 - 110) * 1.15;
         double actual = Fit.idealWeight(height, false);
         double delta = 0.01;
         assertEquals(expected, actual, delta);
@@ -26,7 +26,7 @@ public class FitTest {
     @Test
     public void whenManHeight170ThenWeight80() {
         short height = 170;
-        double expected = (170 - 100) * 1.15; // 80.50
+        double expected = (170 - 100) * 1.15;
         double actual = Fit.idealWeight(height, true);
         double delta = 0.01;
         assertEquals(expected, actual, delta);
@@ -35,7 +35,7 @@ public class FitTest {
     @Test
     public void whenWomanHeight160ThenWeight57() {
         short height = 160;
-        double expected = (160 - 110) * 1.15; // 57.50
+        double expected = (160 - 110) * 1.15;
         double actual = Fit.idealWeight(height, false);
         double delta = 0.01;
         assertEquals(expected, actual, delta);
