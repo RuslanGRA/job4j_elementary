f1e2a1813dca81628b04cbb248f77ee246a45e5f
Ruslan <online0180@mail.ru>
2025-01-21 09:56:36 +0300

    изменение тестов

--- src/test/java/ru/job4j/calculator/FitTest.java
+++ src/test/java/ru/job4j/calculator/FitTest.java
@@ -8,7 +8,7 @@ public class FitTest {
     @Test
     public void whenManHeight187ThenWeight100() {
         short height = 187;
-        double expected = (187 - 100) * 1.15;
+        double expected = 100.05;
         double actual = Fit.idealWeight(height, true);
         double delta = 0.01;
         assertEquals(expected, actual, delta);
@@ -17,7 +17,7 @@ public class FitTest {
     @Test
     public void whenWomanHeight170ThenWeight69() {
         short height = 170;
-        double expected = (170 - 110) * 1.15;
+        double expected = 69.00;
         double actual = Fit.idealWeight(height, false);
         double delta = 0.01;
         assertEquals(expected, actual, delta);
@@ -26,7 +26,7 @@ public class FitTest {
     @Test
     public void whenManHeight170ThenWeight80() {
         short height = 170;
-        double expected = (170 - 100) * 1.15;
+        double expected = 80.50;
         double actual = Fit.idealWeight(height, true);
         double delta = 0.01;
         assertEquals(expected, actual, delta);
@@ -35,7 +35,7 @@ public class FitTest {
     @Test
     public void whenWomanHeight160ThenWeight57() {
         short height = 160;
-        double expected = (160 - 110) * 1.15;
+        double expected = 57.50;
         double actual = Fit.idealWeight(height, false);
         double delta = 0.01;
         assertEquals(expected, actual, delta);
